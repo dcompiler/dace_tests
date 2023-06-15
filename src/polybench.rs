b4308bdd644444208104fba0c244db2b29efbba5
Yekai Pan <51003352+QClr@users.noreply.github.com>
2023-06-15 14:13:08 -0400

    Update polybench.rs

--- src/polybench.rs
+++ src/polybench.rs
@@ -1,8 +1,6 @@
 use dace::ast::Node;
 use std::rc::Rc;
 
-
-
 fn trmm_trace(M: usize, N:usize) -> Rc<Node> {
 
     let i_loop_ref = Node::new_single_loop("i", 0, M as i32);
@@ -36,6 +34,55 @@ fn trmm_trace(M: usize, N:usize) -> Rc<Node> {
     i_loop_ref
 }
 
+fn _2mm(NI: usize, NJ: usize, NK: usize, NL: usize) -> Vec<Rc<Node>> {
+    let s_ref_tmp = Node::new_ref("tmp", vec![NI, NJ], |ijk| {
+        vec![ijk[0] as usize, ijk[1] as usize]
+    });
+    let s_ref_a = Node::new_ref("a", vec![NI, NK], |ijk| {
+        vec![ijk[0] as usize, ijk[2] as usize]
+    });
+    let s_ref_b = Node::new_ref("b", vec![NK, NJ], |ijk| {
+        vec![ijk[2] as usize, ijk[1] as usize]
+    });
+    let s_ref_c = Node::new_ref("c", vec![NL, NJ], |ijk| {
+        vec![ijk[3] as usize, ijk[1] as usize]
+    });
+    let s_ref_d = Node::new_ref("d", vec![NI, NL], |ijk| {
+        vec![ijk[0] as usize, ijk[3] as usize]
+    });
+
+    let knk_loop_ref = Node::new_single_loop("k", 0, NK as i32);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_a);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_b);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_tmp);
+    
+    let jnj_loop_ref = Node::new_single_loop("j", 0, NJ as i32);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_tmp);
+    Node::extend_loop_body(&knk_loop_ref, &knk_loop_ref);
+    
+    let ini_loop_ref1 = Node::new_single_loop("i", 0, NI as i32);
+    Node::extend_loop_body(&ini_loop_ref1, &jnj_loop_ref);
+
+    let knj_loop_ref = Node::new_single_loop("k", 0, NJ as i32);
+    Node::extend_loop_body(&knj_loop_ref, &s_ref_tmp);
+    Node::extend_loop_body(&knj_loop_ref, &s_ref_c);
+    Node::extend_loop_body(&knj_loop_ref, &s_ref_d);
+    
+    let jnl_loop_ref = Node::new_single_loop("j", 0, NL as i32);
+    Node::extend_loop_body(&jnj_loop_ref, &s_ref_d);
+    Node::extend_loop_body(&jnj_loop_ref, &knj_loop_ref);
+
+    let ini_loop_ref2 = Node::new_single_loop("i", 0, NI as i32);
+    Node::extend_loop_body(&ini_loop_ref2, &jnl_loop_ref);
+
+
+    let mut vec = Vec::new();
+    vec.push(ini_loop_ref1);
+    vec.push(ini_loop_ref2);
+
+    vec
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,4 +93,10 @@ mod tests {
         let ast = trmm_trace(M, N);
         assert_eq!(ast.node_count(), 7);
     }
+    
+    fn _2mm_test() {
+        let ast = _2mm(1024, 1024, 1024, 1024);
+        assert_eq!(ast.len(), 2);
+    }
+
 }
